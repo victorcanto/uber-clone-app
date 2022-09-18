a47fb0869036bf3d70c50f97f9dc5bc4517e4f00
Victor Canto <iamvictorcanto@gmail.com>
2022-09-18 19:36:53 -0300

    feat: added App navigation and redux Provider

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Platform } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
+import { store } from '@store/store';
+import { HomeScreen } from '@screens/home-screen';
+import { MapScreen } from '@screens/map-screen';
+import { RootStackParamList } from '@navigation/types';
 
 export default function App() {
+  const RootStack = createNativeStackNavigator<RootStackParamList>();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={Platform.OS === 'ios' ? -64 : 0}
+            style={{ flex: 1 }}>
+            <RootStack.Navigator initialRouteName='HomeScreen'>
+              <RootStack.Screen
+                name='HomeScreen'
+                component={HomeScreen}
+                options={{ headerShown: false }}
+              />
+              <RootStack.Screen
+                name='MapScreen'
+                component={MapScreen}
+                options={{ headerShown: false }}
+              />
+            </RootStack.Navigator>
+          </KeyboardAvoidingView>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
