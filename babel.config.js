dd520b50c010d3ea0bda7027594917e7bcbfcd27
Victor Canto <iamvictorcanto@gmail.com>
2022-09-18 19:33:56 -0300

    chore: project setup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,28 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+        },
+      ],
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@components': './src/components',
+            '@navigation': './src/navigation',
+            '@screens': './src/screens',
+            '@store': './src/store',
+          },
+        },
+      ],
+    ],
   };
 };
